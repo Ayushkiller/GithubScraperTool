947c4e49c3fabf9d3eaec503cc72d4be56ccd693
Ayushkiller <152204318+Ayushkiller@users.noreply.github.com>
2024-09-09 22:26:04 +0530

    Update GitHubReviewerToolApplication.java

--- src/main/java/com/example/githubreviewertool/GitHubReviewerToolApplication.java
+++ src/main/java/com/example/githubreviewertool/GitHubReviewerToolApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitHubReviewerToolApplication {
 
+    /**
+     * The main entry point of the GitHub Reviewer Tool application.
+     *
+     * @param  args  command line arguments
+     * @return      none
+     */
     public static void main(String[] args) {
         SpringApplication.run(GitHubReviewerToolApplication.class, args);
     }
